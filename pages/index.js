e4fbf30dc8068f637fc21c914386bd8b60ca105d
ayusha2806 <anandayush2806@gmail.com>
2024-02-13 14:07:29 +0530

    Time to become a Full Stack Developer

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,49 @@
-import { useEffect, useState } from "react";
-import Layout from "../components/layout/Layout";
-import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from 'mongodb';
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 10, 12345 Some City",
-    description: "This is a second meetup!",
-  },
-];
+import MeetupList from '../components/meetups/MeetupList';
 
 function HomePage(props) {
-  
-  // useEffect(() => {
-  //   setLoadedMeetups(DUMMY_MEETUPS);
-  // }, []);
-  
-  return (
-    <Layout>
-      <MeetupList meetups={props.meetups} />;
-    </Layout>
-  );
+  return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getServerSideProps(context){
-  const req =context.req;
-  const res = context.res;
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
 
+//   // fetch data from an API
 
-// export async function getStaticProps(){
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://ayush_530:Ayush2anand@cluster0.eezvjo5.mongodb.net/?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
   
-  return{
-    props:{
-      meetups:DUMMY_MEETUPS
-    }
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title || 'No Title',
+        address: meetup.address || 'No Address',
+        image: meetup.image || 'No Image',
+        description: meetup.description || 'No Description',
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
   };
 }
 
